5e99a2c07a95aa7066adeb323a94263b3e4381a1
fedoroff94 <artsemfedziankou@gmail.com>
2021-11-17 11:11:30 +0300

    init: start handpose_app

--- index.js
+++ index.js
@@ -1,116 +1,130 @@
-//variables
-const videoElement = document.getElementsByClassName('input_video')[0];
-const canvasElement = document.getElementsByClassName('output_canvas')[0];
-const canvasCtx = canvasElement.getContext('2d');
-const rectangle = document.getElementsByClassName('inner-box')[0];
-const fpsCounter = document.getElementById('fps-counter');
+// //variables
+// const videoElement = document.getElementsByClassName('input_video')[0];
+// const canvasElement = document.getElementsByClassName('output_canvas')[0];
+// const canvasCtx = canvasElement.getContext('2d');
+// const rectangle = document.getElementsByClassName('inner-box')[0];
+// const fpsCounter = document.getElementById('fps-counter');
 
-const rectanglePosition = {
-  top: window.pageYOffset + rectangle.getBoundingClientRect().top,
-  left: window.pageXOffset + rectangle.getBoundingClientRect().left,
-  right: window.pageXOffset + rectangle.getBoundingClientRect().right,
-  bottom: window.pageYOffset + rectangle.getBoundingClientRect().bottom
-}
+// const rectanglePosition = {
+//   top: window.pageYOffset + rectangle.getBoundingClientRect().top,
+//   left: window.pageXOffset + rectangle.getBoundingClientRect().left,
+//   right: window.pageXOffset + rectangle.getBoundingClientRect().right,
+//   bottom: window.pageYOffset + rectangle.getBoundingClientRect().bottom
+// }
 
-//work with canvas
-function onResults(results) {
-  canvasCtx.save();
-  canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
-  canvasCtx.drawImage(
-    results.image, 0, 0, canvasElement.width, canvasElement.height);
-  if (results.multiHandLandmarks) {
-    for (const landmarks of results.multiHandLandmarks) {
-      drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS,
-        { color: 'red', lineWidth: 2 });
-      drawLandmarks(canvasCtx, landmarks, { color: 'yellow', lineWidth: 1 });
+// let getFingerCoordinates = (finger) => {
 
-      let getFingerCoordinates = () => {
+//   let fingerX = 1280 * finger.x;
+//   let fingerY = 720 * finger.y;
 
-        let fingerX = 1280 * landmarks[8].x;
-        let fingerY = 720 * landmarks[8].y;
+//   let fingerCoordinates = {
+//     top: window.pageYOffset + fingerY,
+//     left: 1280 - (window.pageXOffset + fingerX),
+//     right: 1280 - (window.pageXOffset + fingerX),
+//     bottom: window.pageYOffset + fingerY
+//   }
 
-        let fingerCoordinates = {
-          top: window.pageYOffset + fingerY,
-          left: 1280 - (window.pageXOffset + fingerX),
-          right: 1280 - (window.pageXOffset + fingerX),
-          bottom: window.pageYOffset + fingerY
-        }
+//   return fingerCoordinates;
 
-        if (landmarks[8]) {
-          if (fingerCoordinates.bottom > rectanglePosition.top &&
-            fingerCoordinates.top < rectanglePosition.bottom &&
-            fingerCoordinates.right > rectanglePosition.left &&
-            fingerCoordinates.left < rectanglePosition.right) {
+// }
 
-            rectangle.classList.add('color')
-          } else {
-            rectangle.classList.remove('color')
-          }
-        }
+// //work with canvas
+// function onResults(results) {
+//   canvasCtx.save();
+//   canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+//   canvasCtx.drawImage(
+//     results.image, 0, 0, canvasElement.width, canvasElement.height);
+//   if (results.multiHandLandmarks) {
+//     for (const landmarks of results.multiHandLandmarks) {
+//       drawConnectors(canvasCtx, landmarks, HAND_CONNECTIONS,
+//         { color: 'red', lineWidth: 2 });
+//       drawLandmarks(canvasCtx, landmarks, { color: 'yellow', lineWidth: 1 });
 
-      }
+//       let initiateFinger8 = getFingerCoordinates(landmarks[8]);
+//       let initiateFinger4 = getFingerCoordinates(landmarks[4]);
 
-      getFingerCoordinates();
+//       if (landmarks[8] && landmarks[4]) {
+//         if ((initiateFinger8.bottom > rectanglePosition.top &&
+//           initiateFinger8.top < rectanglePosition.bottom &&
+//           initiateFinger8.right > rectanglePosition.left &&
+//           initiateFinger8.left < rectanglePosition.right) &&
+//           (initiateFinger4.bottom > rectanglePosition.top &&
+//             initiateFinger4.top < rectanglePosition.bottom &&
+//             initiateFinger4.right > rectanglePosition.left &&
+//             initiateFinger4.left < rectanglePosition.right) &&
+//             (initiateFinger4.top - initiateFinger8.top <= 70 ||
+//               initiateFinger4.top - initiateFinger8.top <= -70) &&
+//               (initiateFinger4.left - initiateFinger8.left <= 70 ||
+//                 initiateFinger4.left - initiateFinger8.left <= -70)) {
 
-    }
+//           rectangle.classList.add('color')
 
-  }
-  canvasCtx.restore();
-}
+//         } else {
 
-//something from mediapipe
-const hands = new Hands({
-  locateFile: (file) => {
-    return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
-  }
-});
+//           rectangle.classList.remove('color')
+//         }
 
-hands.setOptions({
-  maxNumHands: 2,
-  modelComplexity: 1,
-  minDetectionConfidence: 0.5,
-  minTrackingConfidence: 0.5
-});
+//       }
 
-hands.onResults(onResults);
+//     }
+//   }
+//   canvasCtx.restore();
+// }
 
-//work with camera
-const camera = new Camera(videoElement, {
-  onFrame: async () => {
-    await hands.send({ image: videoElement });
-  },
-  width: 1280,
-  height: 720
-});
+// //something from mediapipe
+// const hands = new Hands({
+//   locateFile: (file) => {
+//     return `https://cdn.jsdelivr.net/npm/@mediapipe/hands/${file}`;
+//   }
+// });
 
-camera.start();
+// hands.setOptions({
+//   maxNumHands: 2,
+//   modelComplexity: 1,
+//   minDetectionConfidence: 0.5,
+//   minTrackingConfidence: 0.5
+// });
+
+// hands.onResults(onResults);
+
+// //work with camera
+// const camera = new Camera(videoElement, {
+//   onFrame: async () => {
+//     await hands.send({ image: videoElement });
+//   },
+//   width: 1280,
+//   height: 720
+// });
+
+// camera.start();
 
 
-//FPS counter
-let frameCount = function _fc(timeStart){
-        
-  let now = performance.now();
-  let duration = now - timeStart;
-  
-  if(duration < 100){
-      
-      _fc.counter++;
-      
-  } else {
-                
-      _fc.fps = _fc.counter * 10;
-      _fc.counter = 0;
-      timeStart = now; 
-      fpsCounter.innerHTML = `${_fc.fps + ' fps'}`;
+// //FPS counter
+// let frameCount = function _fc(timeStart) {
 
-  }
-  requestAnimationFrame(() => frameCount(timeStart)); 
-}
+//   let now = performance.now();
+//   let duration = now - timeStart;
 
-frameCount.counter = 0;
-frameCount.fps = 0;
+//   if (duration < 100) {
 
-frameCount(performance.now())
+//     _fc.counter++;
+
+//   } else {
+
+//     _fc.fps = _fc.counter * 10;
+//     _fc.counter = 0;
+//     timeStart = now;
+//     fpsCounter.innerHTML = `${_fc.fps + ' fps'}`;
+
+//   }
+
+//   requestAnimationFrame(() => frameCount(timeStart));
+// }
+
+// frameCount.counter = 0;
+// frameCount.fps = 0;
+
+// frameCount(performance.now())
 
 
 
